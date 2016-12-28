1ca108c17fc2e18e321e62017da78ef4de34f87d
Ruijia Yang <ry2277@columbia.edu>
2016-12-27 21:52:13 -0500

    put REST API in server.js instead of api.js; tested using postman, API calls are ok

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 /* index.js is the entry point for running the web application */
 
-var express = require("express");
-var wagner = require("wagner-core");
+var server = require('./server');
 
-/* bootstrap Mongoose models */
-require("./models")(wagner);
-
-var app = express();
-
-/* bootstrap the application using the api.js file 
-app.use() is for Express subrouter functionality
-*/
-app.use("/api/v1", require("./api")(wagner));
-
-app.listen(3000);
-console.log("Listening on port 3000!");
+server().listen(3000);
+console.log("Server listening on port 3000!");
--- server.js
+++ server.js
@@ -1,24 +1,96 @@
 var express = require('express');
 
 module.exports = function() {
+  var app = express();
 
-	/* creates an express application */
-	var app = express();
+  
+  /*************** create the users above ***************/
+  // Create (POST)
+  app.post("/signup", function(req, res) {
+    res.send("Got a POST request for a user");
+  });
 
-	app.get('/', function(req, res) {
-		res.send('Soundscape');	// res = response
-	});
+  /*************** read ***************/
+  // list all users
+  app.get("/allusers", function(req, res) {
+    res.send("Got a GET request for a list of all users");
+  });
 
-	/* app.get(path, callback)
-	routes HTTP GET requests to the specified path with the specified 
-	callback functions
-	*/
-	app.get('/user/:user/playlist/:playlist', function(req, res) {
-		/* req.params.user gives you access to whatever string is in the :user
-		portion of the url */
-		res.send('You searched in ' + req.params.user + ' for playlist ' + 
-			req.params.playlist);
-	});
+  // list a specified user
+  app.get("/user/:user", function(req, res) {
+    res.send("Got a GET request for the profile of user " + req.params.user + ".");
+  });
 
-	return app;
+  /*************** edit a user ***************/
+  // edit user metadata
+  app.put("/user/:user/edit", function(req, res) {
+    res.send("Got a PUT request for user " + req.params.user + ".");
+  });
+
+  /*************** delete a user ***************/
+  app.delete("/user/:user/edit", function(req, res) {
+    res.send("Got a DELETE request for user " + req.params.user + ".");
+  });
+
+  /*****************************************************************************
+  ******************************* PLAYLIST LEVEL *******************************
+  ******************************************************************************/
+  
+  /*************** create a playlist ***************/
+  app.post("/user/:user/create_playlist", function(req, res) {
+    res.send("Got a POST request from user " + req.params.user + "to create a new playlist.");
+  });
+
+  /*************** read a playlist ***************/
+  // read a specific user's playlist
+  app.get("/user/:user/playlist/:playlist_title", function(req, res) {
+    res.send("Got a GET request for user " + req.params.user + "'s playlist " +
+      req.params.playlist_title);
+  });
+
+  // read all playlists
+  app.get("/allplaylists", function(req, res) {
+    res.send("Got a GET request for all playlists");
+  });
+
+  /*************** edit a playlist ***************/
+  // edit playlist metadata
+  app.put("/user/:user/playlist/:playlist_title/edit", function(req, res) {
+    res.send("Got a PUT request for user " + req.params.user + "'s playlist " +
+      req.params.playlist_title);
+  });
+
+  /*************** delete a playlist ***************/
+  app.put("/user/:user/playlist/:playlist_title/edit", function(req, res) {
+    res.send("Got a DELETE request for user " + req.params.user + "'s playlist " +
+      req.params.playlist_title);
+  });
+
+  /*****************************************************************************
+  ******************************** AUDIO LEVEL *********************************
+  ******************************************************************************/
+  // add a song to a playlist (create)
+  app.post("/user/:user/playlist/:playlist_title/edit", function(req, res) {
+    res.send("Got a CREATE request for an audio file in user " + req.params.user + "'s playlist " +
+      req.params.playlist_title);
+  });
+
+  // open up a song and read its description  (read) - N/A
+
+  // edit song metadata (update)
+  app.put("/user/:user/playlist/:playlist_title/edit", function(req, res) {
+    res.send("Got a PUT request for an audio file in user " + req.params.user + "'s playlist " +
+      req.params.playlist_title);
+  });
+
+  // delete a song from a playlist (delete)
+  app.delete("/user/:user/playlist/:playlist_title/edit", function(req, res) {
+    res.send("Got a DELETE request for an audio file in user " + req.params.user + "'s playlist " +
+      req.params.playlist_title);
+  });
+
+  /* return the Express router so higher-level apps can include the router
+  using app.use() */
+  return app;
+
 };

89796da5fe66dacdb404171d0f587d58784f0fa2
Ruijia Yang <ry2277@columbia.edu>
2016-12-31 00:12:49 -0500

    front end gps-based audio player works

--- backend/makerequest.js
+++ backend/makerequest.js
@@ -1,70 +1,75 @@
 var request = require('request');
 
 // CREATE NEW USER
-/*
 request.post(
     'http://localhost:3000/api/signup',
     { json: {
 	prof: {
-		username: "jordi",
-		picture: "http://canyouactually.com/wp-content/uploads/charlie-snoopy.jpg",
+		username: "mozart",
+		picture: "http://a4.files.biography.com/image/upload/c_fit,cs_srgb,dpr_1.0,h_1200,q_80,w_1200/MTE1ODA0OTcxNzMyNjY1ODY5.jpg",
 		// but the URL https://s-media-cache-ak0.pinimg.com/originals/60/f6/b2/60f6b2b59177878584d427cf9cd7cad0.jpg causes an 
 		// internal server error?
-		bio: "It has been said that astronomy is a humbling and character-building experience",
+		bio: "i am classy",
 		playlists: [
 		{
-			playlist_id: "chinese_songs",
-			playlist_title: "chinese songs",
-			playlist_description: "a collection of chinese songs",
+			playlist_id: "classical_music",
+			playlist_title: "classical music",
+			playlist_description: "a playlist for classical music",
 			audios: 
 			[{
-				audio_title: "yi sheng you ni",
-				audio_file: "http://wavgy.yt-downloader.org/download.php?id=b40d3789ad4040c6badc471f440c9c00",
+				audio_title: "Arrival of the Queen of Sheba",
+				audio_file: "http://www.stephaniequinn.com/Music/Handel%20-%20Entrance%20of%20the%20Queen%20of%20Sheba.mp3",
 				location: {
-					lat: "40.0",
-					lon: "-74.0"
+					lat: "41.484044",
+					lon: "-73.251855"
 				},
-				audio_description: "yi sheng you ni is a chinese song"
+				audio_description: "good song!"
 			},
 			{
-				audio_title: "guang yin de gu shi",
-				audio_file: "http://wavgy.yt-downloader.org/download.php?id=b40d3789ad4040c6badc471f440c9c00",
+				audio_title: "Hungarian Dance - Brahms",
+				audio_file: "http://www.stephaniequinn.com/Music/Hungarian%20Dance.mp3",
 				location: {
-					lat: "41.0",
-					lon: "-75.0"
+					lat: "41.484060",
+					lon: "-73.251818"
 				},
-				audio_description: "this is also the name of a TV show"
-			}]
+				audio_description: ":)"
+			},
+			{
+				audio_title: "Presto - Mozart",
+				audio_file: "http://www.stephaniequinn.com/Music/Mozart%20-%20Presto.mp3",
+				location: {
+					lat: "41.484120",
+					lon: "-73.251560"
+				},
+				audio_description: "good"
+			},
+			{
+				audio_title: "Allegro - Beethoven",
+				audio_file: "http://www.stephaniequinn.com/Music/Allegro%20from%20Duet%20in%20C%20Major.mp3",
+				location: {
+					lat: "41.484160",
+					lon: "-73.251474"
+				},
+				audio_description: "beethoven"
+			},
+			{
+				audio_title: "Spring - Vivaldi",
+				audio_file: "http://www.stephaniequinn.com/Music/Vivaldi%20-%20Spring%20from%20Four%20Seasons.mp3",
+				location: {
+					lat: "41.484160",
+					lon: "-73.251496"
+				},
+				audio_description: ":)"
+			}
+			]
 		},
-		{
-			playlist_id: "test_me",
-			playlist_title: "test me",
-			playlist_description: "around the big apple",
-			audios: 
-			[{
-				audio_title: "a-punk",
-				audio_file: "http://wavgy.yt-downloader.org/download.php?id=b40d3789ad4040c6badc471f440c9c00",
-				location: {
-					lat: "42.4",
-					lon: "-74.0"
-				},
-				audio_description: ""
-			},
-			{
-				audio_title: "empire state of mind",
-				audio_file: "http://wavgy.yt-downloader.org/download.php?id=b40d3789ad4040c6badc471f440c9c00",
-				location: {
-					lat: "41.0",
-					lon: "-75.0"
-				},
-				audio_description: ""
-			}]
-		}]
+		]
 	},
 	auth: {
 		oauth: "hello1"
 	}
-	} }
+	} },
+
     function (error, response, body) {
     	console.log(response.statusCode);
         if (!error && response.statusCode == 200) {
@@ -72,7 +77,6 @@ request.post(
         }
     }
 );
-*/
 
 /*
 // EDIT USER
@@ -159,6 +163,7 @@ request.put(
 */
 
 // DELETE USER PLAYLIST
+/*
 request.delete(
 	"http://localhost:3000/api/user/ruijia/playlist/chinese_songs/edit",
 	{ json: {
@@ -171,3 +176,4 @@ request.delete(
         console.log(body);
     }
 );
+*/
--- frontend/controllers.js
+++ frontend/controllers.js
@@ -21,22 +21,93 @@ exports.UserProfileController = function($scope, $routeParams, $http) {
 
 /* sends HTTP request to the REST API to get data about a playlist */
 exports.PlaylistViewController = function($scope, $routeParams, $http) {
-  console.log($routeParams);
-  // routeParams service = map from route parameters in the AngularJS 
-  // URL to the route parameter values
-  var encodedUser = encodeURIComponent($routeParams.user);
-  var encodedPlaylist = encodeURIComponent($routeParams.playlist);
 
-  $http.
-    get("/api/user/" + encodedUser + "/playlist/" + encodedPlaylist).
-    success(function(data) {
-      console.log("success");
-      console.log(data);
-      $scope.playlist = data;
+  $scope.load = function() {
+    console.log($routeParams);
+    // routeParams service = map from route parameters in the AngularJS 
+    // URL to the route parameter values
+    var encodedUser = encodeURIComponent($routeParams.user);
+    var encodedPlaylist = encodeURIComponent($routeParams.playlist);
 
-    });
+    $http.
+      get("/api/user/" + encodedUser + "/playlist/" + encodedPlaylist).
+      success(function(data) {
+        console.log("success");
+        console.log(data);
+        $scope.playlist = data;
 
+      });
+  };
+
+  $scope.load();
+  
+  // haversine formula: http://www.movable-type.co.uk/scripts/latlong.html
+  $scope.selectClosestAudio = function() {
+    // get current location
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(function(location) {
+        var curLat = location.coords.latitude;
+        var curLon = location.coords.longitude;
+        console.log("lat: " + location.coords.latitude);
+        console.log("lon: " + location.coords.longitude);
+        console.log("accuracy: " + location.coords.accuracy);
+
+        var minDist = -1;
+        var dist = -1;
+        var newClosestAudioSrc = null;
+        for (var i = 0; i < $scope.playlist.audios.length; i++) {
+          iAudio = $scope.playlist.audios[i];
+          dist = Math.sqrt(Math.pow((iAudio.location.lat - curLat), 2) + Math.pow((iAudio.location.lon - curLon), 2));
+          if (minDist == -1 || dist < minDist) {
+            minDist = dist;
+            newClosestAudioSrc = iAudio.audio_file;
+            console.log("newClosestAudio is " + newClosestAudioSrc );
+          }
+        }
+        if (newClosestAudioSrc != $scope.closestAudioSrc)
+          $scope.closestAudioSrc = newClosestAudioSrc;
+      });
+      console.log("$scope.closestAudioSrc is " + $scope.closestAudioSrc );
+    }
+  };
+
+  $scope.playSoundscape = function() {
+    $scope.selectClosestAudio();
+    $scope.closestAudioObject = new Audio($scope.closestAudioSrc);
+    curAudioStr = $scope.closestAudioSrc;
+    setInterval(function() {
+      console.log("re-check audio");
+      $scope.selectClosestAudio();
+      console.log("next audio is: "+ $scope.closestAudioSrc);
+      if (curAudioStr != $scope.closestAudioSrc) {
+          curAudioStr = $scope.closestAudioSrc;
+          ($scope.closestAudioObject).pause();
+          $scope.closestAudioObject = new Audio(curAudioStr);
+          ($scope.closestAudioObject).play();
+        }
+      }, 10000);
+    /*
+    var nextAudioStr = null;
+    $scope.audioSrc = new Audio(curAudioStr); // or keep the same Audio and keep the source? delete old Audio?
+
+    ($scope.audioSrc).play();
+    while (1) {
+      setTimeout(function() {
+        console.out("re-check audio");
+        nextAudioStr = $scope.selectClosestAudio();
+        if (curAudioStr != nextAudioStr) {
+          curAudioStr = nextAudioStr;
+          ($scope.audioSrc).pause();
+          $scope.audioSrc = new Audio(curAudioStr);
+          ($scope.audioSrc).play();
+        }
+      }, 10000);
+    }
+    */
+  }
+  
   setTimeout(function() {
     $scope.$emit('PlaylistViewController');
   }, 0);
+
 };
